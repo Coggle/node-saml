f99ff1e241b1cc5ca1ab3b496d823e1315b8cb6c
James Crosby <james@coggle.it>
2021-07-19 17:10:18 +0100

    build and commit build

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import type { CacheItem, CacheProvider } from "./inmemory-cache-provider";
+import { SAML } from "./saml";
+import { MandatorySamlOptions, Profile, SamlConfig, SamlOptions } from "./types";
+export { SAML, CacheItem, CacheProvider, SamlOptions, MandatorySamlOptions, Profile, SamlConfig };
